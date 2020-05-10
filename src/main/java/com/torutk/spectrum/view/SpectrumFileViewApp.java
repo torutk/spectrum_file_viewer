c40afdfddff25d14f56999e16a3a29a3d74ddfc6
TAKAHASHI,Toru <torutk@gmail.com>
2020-05-10 15:16:45 +0900

    Add simple main JavaFX class and JPMS module declaration
    - deleted main class and main test class generated by gradle init.

---
+++ src/main/java/com/torutk/spectrum/view/SpectrumFileViewApp.java
@@ -0,0 +1,12 @@
+package com.torutk.spectrum.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpectrumFileViewApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.torutk.spectrum {
+    requires javafx.graphics;
+    opens com.torutk.spectrum.view to javafx.graphics;
+}
